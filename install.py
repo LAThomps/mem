7ee565d98621a6d7487a8d36313639db04a02a32
LThomps <luke.a.thompson3@gmail.com>
2025-06-21 16:57:16 -0400

    added psudo code to install.py

---
+++ install.py
@@ -0,0 +1,17 @@
+import os 
+import subprocess
+
+# ask for input for venv with psutil installed
+# verify valid python venv and has psutil instlled
+# python.exe at path/bin/
+# psutil at path/lib/python*/site-packages/
+# have to run chmod +x on mem.py
+# copy it to /usr/local/bin/mem.py [[sudo]]
+# rename it mv mem.py mem
+# print all done statement
+def main():
+    print(__file__)
+    return 
+
+if __name__ == "__main__":
+    main()
